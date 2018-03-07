3996a1003e0bed9fb5c03b1cf9d2791ae5d91cb6
Rafael Silveira <rafaelssilveira@outlook.com>
2018-03-07 17:59:52 -0300

    a

---
+++ The Development Web Bootcamp-Colt Steele/BlueWhite.js
@@ -0,0 +1,13 @@
+var body = document.querySelector("body");
+var isBlue = false;
+
+setInterval(function() {
+    if (isBlue){
+        body.style.background ="white";
+    } else {
+        body.style.background = "#3498db";
+
+    }
+    isBlue = !isBlue;
+    
+},1000)
