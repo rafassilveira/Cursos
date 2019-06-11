d3608842b55be99d9f02eb7ebb575b61be5f04a8
Rafael Silveira <rafaelssilveira@outlook.com>
2019-06-10 23:23:49 -0300

    Termianr Desafio 5

--- StudyFlow/javaScript-ninja/challenge-05/challenge-05.js
+++ StudyFlow/javaScript-ninja/challenge-05/challenge-05.js
@@ -2,17 +2,21 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var array = [1,true,false,"Rafael",0]
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function Myfunc(arg) {
+    return arg
+    
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+> Myfunc(array[1])
+true
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +24,22 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function myFunc2 (arg1,arg2) {
+    return arg1[arg2]
+  }
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var array = [1, true, false, "Rafael", 0]
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+> myFunc2(array, 2)
+false
+  >
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,19 +55,49 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(arg1) { 
+        var  livro = {
+            livro1:{
+                quantidadePaginas:30,
+                autor:'JK',
+                editora:"Leya"
+            },        
+            livro2: {
+            quantidadePaginas: 40,
+            autor: 'JK2',
+            editora: "Leya2"
+            },
+            livro3: {
+            quantidadePaginas: 50,
+            autor: 'JK3',
+            editora: "Leya3"
+            },
+        }
+        
+        if (arg1) {
+            return arg1
+            
+        }else{
+            return livro
+        }
+       
+   
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+> book(livro)
+{ HarryPotter: { quantidadePaginas: 30, autor: 'JK', editora: 'Leya' },
+  livro2: { quantidadePaginas: 40, autor: 'JK2', editora: 'Leya2' },
+  livro3: { quantidadePaginas: 50, autor: 'JK3', editora: 'Leya3' } }
+>
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
