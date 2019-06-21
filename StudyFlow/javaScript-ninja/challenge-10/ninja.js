733cc0af9b60361bd85aa3e789188e70c1a98f0b
Rafael Silveira <rafaelssilveira@outlook.com>
2019-06-20 21:01:47 -0300

    Desafio 10 incompletdo

---
+++ StudyFlow/javaScript-ninja/challenge-10/ninja.js
@@ -0,0 +1,27 @@
+
+//*Wraper Objects*
+//-Contrutores-Criam novos objetos
+// Com construtores:
+var name = new String('Teste');
+var age = new Number(30);
+var ninja = new Boolean(false);
+
+//Sem construtores
+var name =  String('Teste');
+var age =  Number(30);
+var ninja =  Boolean(false);
+
+console.log(age)
+
+//typeof
+typeof undefined; //undefined
+typeof function () {} // function
+typeof true;// boolean
+typeof 10;// number
+typeof 'JS Ninja';// string
+
+//Quaçquer outro objeto que não seja uma functions ou null retorna 'object'
+//Idela é usar para valores primitivos
+
+
+ 
