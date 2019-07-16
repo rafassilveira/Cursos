20531e53c9ef28bea0ea85d7b0e756e4651522ae
Rafael Silveira <rafaelssilveira@outlook.com>
2019-07-16 19:55:29 -0300

    Desafio 15 finalizado! :))

--- StudyFlow/javaScript-ninja/challenge-15/challenge-15.js
+++ StudyFlow/javaScript-ninja/challenge-15/challenge-15.js
@@ -28,26 +28,21 @@ function Person (name, lastName,age) {
   this.age = age;
       this.getFullName = function () {
 
-        return person.name + " " + person.lastName
+        return this.name + " " + this.lastName
 
       }
+      this.getAge = function () {
+          return this.age
+        }
+      this.addAge = function () {
+        //arguments serve para pegar os parametros passados sem serem declarados
+        //ele funciona como array, mas não é um
+        //[0] pegerá o primeiro paramentro passado
+        this.age +=arguments[0]
+        //this representa o objeto a ser estanciado
+        return this;
+        }
  }
-
- var person = new Person('rafael','silveira',30)
-
-  /*person.getFullName = function () {
-    
-    return person.name + " " + person.lastName
-    
-  }*/
-
- console.log(person.getFullName());
- 
-
-})(); //fim IIFE
-
-
-
 
 /*
 Crie 3 novos objetos usando o construtor acima. Os objetos serão novas
@@ -56,25 +51,50 @@ parâmetros corretamente para o construtor para criar as novas pessoas.
 Mostre as 3 novas pessoas criadas no console (Um console.log por pessoa).
 */
 console.log( 'Novas pessoas criadas à partir de Person:' );
-// ?
+
+var rafael = new Person ('Rafael','Silveira',30)
+var pam = new Person('Pâmela', 'Silva', 31)
+var bulma = new Person('bulma', 'bulldog', 3)
 
 /*
 Mostre no console o nome completo de cada pessoa.
 */
 console.log( '\nNomes das pessoas:' );
-// ?
+console.log(rafael.getFullName());
+console.log(pam.getFullName());
+console.log(bulma.getFullName());
 
 /*
 Mostre no console as idades de cada pessoa, com a frase:
 - "[NOME COMPLETO] tem [IDADE] anos."
 */
 console.log( '\nIdade das pessoas:' );
-// ?
 
+console.log(rafael.getFullName() + " tem: " + rafael.getAge() +" anos" );
+console.log(pam.getFullName() + " tem: " + pam.getAge() +" anos" );
+console.log(bulma.getFullName() + " tem: " + bulma.getAge() +" anos" );
 /*
 Adicione alguns anos à cada pessoa, e mostre no console a nova idade de
 cada um. A frase deverá ser no formato:
 - "[NOME COMPLETO] agora tem [NOVA IDADE] anos."
 */
 console.log( '\nNova idade das pessoas:' );
-// ?
+rafael.addAge(5);
+pam.addAge(4);
+
+
+console.log(rafael.getFullName() + " tem: " + rafael.getAge() +" anos" );
+console.log(pam.getFullName() + " tem: " + pam.getAge() +" anos" );
+//encadeando metodos
+console.log(bulma.getFullName() + " tem: " + bulma.addAge(3).getAge() +" anos" );
+
+})(); //fim IIFE
+
+
+
+
+
+
+
+
+
