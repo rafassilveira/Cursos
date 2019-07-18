369a8b3cfbd64d2c019138381100516d73f77cc2
Rafael Silveira <rafaelssilveira@outlook.com>
2019-07-18 20:00:00 -0300

    Terminar aula 96-use strict

---
+++ StudyFlow/javaScript-ninja/challenge-16/ninja.js
@@ -0,0 +1,15 @@
+//USE STRICT;
+// 1-NÃO DAR PARA USAR COM O 'THIS'
+// 2- NÃO DÁ PRA USAR COM O 'WITH'
+(function () {
+    'use strict'
+    //O 'use strict' vai fazer com que as variavéis,
+    //daque em diante sejam usada de forma estritra
+    //ou seja somente localmente nessa função
+    //por isso ele nao permite declarar variavel sem  'var'
+    var myName = 'Rafael Silveira';
+    console.log(myName)
+}
+
+)
+
