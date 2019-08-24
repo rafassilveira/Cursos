6d25130f87e5879e39c4c0cc45e212a1952d33af
Rafael Silveira <rafaelssilveira@outlook.com>
2019-08-24 19:36:02 -0300

    Desafio 21 completo

--- StudyFlow/javaScript-ninja/challenge-21/challenge-21.js
+++ StudyFlow/javaScript-ninja/challenge-21/challenge-21.js
@@ -14,4 +14,61 @@ Utilize o atributo data-js para nomear o campo e os botões. Você pode
 usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
 dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
 */
-// ?
+(function (win,doc) {
+    
+    'use strict'
+
+    let $timer = doc.querySelector('[data-js="timer"]');
+    let $start = doc.querySelector('[data-js="start"]');    
+    let $reset = doc.querySelector('[data-js="reset"]');
+    let $stop = doc.querySelector('[data-js="stop"]');
+    let interval; 
+    
+    /*Versão do professor */
+    $start.addEventListener('click',startTimer,false)
+    $stop.addEventListener('click',stopTimer,false)
+    $reset.addEventListener('click',resetTimer,false)
+
+    function startTimer() {
+        $timer.value = +$timer.value + 1;
+        interval = setTimeout(startTimer, 1000)
+      }
+
+    function stopTimer () {  
+        clearTimeout(interval)
+
+    }
+    function resetTimer() { 
+        $timer.value = 0
+        stopTimer();
+
+    }
+    /*
+    Minha versão
+
+    $start.addEventListener('click',function () {
+        function timer () {
+            $timer.value = +$timer.value +1;
+
+            interval = setTimeout(timer,1000)
+
+          }
+          timer()
+
+      },false)
+
+    $stop.addEventListener('click', function () {
+        clearTimeout(interval)
+      },false)
+    
+      $reset.addEventListener('click',function () {  
+          clearTimeout(interval)
+          $timer.value = 0
+         
+
+      },false)
+
+      */
+
+     })(window, document)
+
