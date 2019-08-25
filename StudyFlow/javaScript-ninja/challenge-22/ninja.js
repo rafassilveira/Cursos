7decc7a35c53c47a6f2343282af12d2e2b4bfc8a
Rafael Silveira <rafaelssilveira@outlook.com>
2019-08-25 20:43:59 -0300

    Anatações feitas,começar desafio 22

--- StudyFlow/javaScript-ninja/challenge-22/ninja.js
+++ StudyFlow/javaScript-ninja/challenge-22/ninja.js
@@ -28,7 +28,7 @@
                 (function () {
                   'use strict'
 
-                  function myFunction(params) {
+                  function myFunction(a.b.c.d) {
                     console.log(this.lastname)
 
                   }
@@ -41,15 +41,66 @@
                     lastname: 'Santos'
 
                   }
+                  //Primeiro parametro é sempre o this , o resto segue os parametros da
+                  função
+                  console.log(myFunction.call(obj2,'a','b','c','d'));
+                  //quando não for necessario passar o this
+                  console.log(myFunction.call(null, 'a', 'b', 'c', 'd'));
+                  //melhor opção
+                  console.log(myFunction.call(myFunction, 'a', 'b', 'c', 'd'));
+        .apply
+            Tem a mesma funcionalidade que o '.call', porém com o a'.apply' podemos
+            passar uma array como parametrosm cada item do array vai ser uma parametro da função
 
-                  console.log(myFunction.call(obj2));
+            Exemplo:
+                  var arr=[1,2,3,4,5]
+                  console.log(myFunction.apply(myFunction, arr));
+                  //é possivel passar um array direto
+                  console.log(myFunction.apply(myFunction,['a','b','c','d'] ));
 
                 })
-               
+        .prototype
+                //Serve para estender funcionalidade de contrutores
+                (function () {
+                  'use strict'
+
+                  function MyFunction(name, lastName) {
+                    this.name = name;
+                    this.lastName = lastName;
+                  }
+                  MyFunction.prototype.fullname = function () {
+                    return this.name + ' ' + this.lastName;
+                  }
+                  var rafael = new MyFunction('Rafael', 'Silveira')
+                  console.log(rafael.fullname());
+
+                })()
+        Array Like
+                São métodos que retornam item como se fosse array,mas na verdade não são
+                Logo eles não aceitam alguns metodos do js.
+
+                Para contonar isso existem algumas gambiarras:
+                Exemplo:
 
 
 
 
 
  */
+
+ function myFunction() {
+    Array.prototype.forEach.call(arguments, function (item,index) {
+      console.log(item);
+      
+      })
+   }
+   myFunction(1,2,3,5,6)
+
+    function myFunction() {
+      Array.prototype.reduce.call(arguments, function (acumulado,atual, index) {
+        console.log(acumulado + atual);
+
+      })
+    }
+    myFunction(1, 2, 3, 5, 6)
 
