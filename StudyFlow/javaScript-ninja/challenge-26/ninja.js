e1226af18e8e8490106966c05e4625c9e6275564
Rafael Silveira <rafaelssilveira@outlook.com>
2019-08-29 09:19:21 -0300

    anotações aula 26

--- StudyFlow/javaScript-ninja/challenge-26/ninja.js
+++ StudyFlow/javaScript-ninja/challenge-26/ninja.js
@@ -42,7 +42,68 @@
     .hasAttribute(name)
       Retorna false ou true se existir algun tributo passado pro parametro
     .hasAttributes()
-    Retorna false ou true se existir algun tributo 
+      Retorna false ou true se existir algun tributo
+    .appendChild(child)
+      Ele coloca um filho
+      var = $mainContent = document.queSelector('.main-content)
+      var = $mainHeader = document.queSelector('.main-Header)
+      $mainContent.appemdChild($mainHeader)
+    .indertBefore(node, beforeWhom)
+      Insere antes de uma tag
+      var = $mainContent = document.queSelector('.main-content)
+      var = $mainHeader = document.queSelector('.main-Header)
+      var $firstText = $mainContent.firstChild;
+      $mainContent.insertBefore($mainHeader,$firstText);
+    .cloneNode(boolean)
+      Se passar como true ele clona a tag inteira, se passar false nao clona
+      var = $mainContent = document.queSelector('.main-content)
+      var = $mainHeader = document.queSelector('.main-Header)
+      var $firstText = $mainContent.firstChild;
+      var $cloneMainHeader.appendChild($cloneMainHeader)
+      $mainContent.appendChild($cloneMainHeader);
+    .hasChildNodes()
+      Verifica se o elemento tem algum filho(texto conta como filho)
+    .removeChild(child)
+      $mainContent.removeChild($cloneMainHeader);
+    .replaceChild(new,old)
+      Troca um elementro pelo outro
+       var = $mainContent = document.queSelector('.main-content)
+       var = $mainHeader = document.queSelector('.main-Header)
+       var $firstText = $mainContent.firstChild;
+       var $cloneMainHeader.replaceChild($cloneMainHeader,$mainHeader)
+    document.createTextNode(text)
+      //Cria um text emum elemento
+      var newTextNode = document.createTextNode('Opa!');
+      $main.appendChild(newTextNode)
+    .createElement('p')
+      Cria um novo elemento
+      var newTextNode = document.createTextNode('Opa!');
+      var $newp = document.createElement('p')
+      //Insere o texto
+      $newP.appendChild(newTextNode);
+      //Insere a tag
+      $main.appendChild($newP)
+
+
+    .getAttribute
+      Pega um atributo (classe,id...), sempre retornar strng
+      var = $main = document.queSelector('.main)
+      $main.getAttribute('data-js');
+
+    .setAttribute(attr,value)
+      Inclui um atributo
+      var = $main = document.queSelector('.main)
+      $main.setAttribute('data-js','data-js-main');
+
+
+
+
+
+
+
+
+
+
 
 
 
