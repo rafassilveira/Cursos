816a3134ba17a1326443001e8eea88d14f3f0222
Rafael Silveira <rafaelssilveira@outlook.com>
2019-08-28 22:50:57 -0300

    anotações aula 26

---
+++ StudyFlow/javaScript-ninja/challenge-26/ninja.js
@@ -0,0 +1,55 @@
+/*
+    --DOM--
+
+  .parentNode
+    Pega o nó pai
+      var = $main = document.queSelector('.main)
+      $main.parenteNode;
+  .childNodes
+    Pega nó fihos,no codigo HTML, cada enter  ou espaçamento conta como um nó
+    var = $main = document.queSelector('.main)
+        $main.parenteNode;
+  .firstChild
+    Pega o primeiro filho,
+    var = $main = document.queSelector('.main)
+        $main.firstChild;
+  .lasttChild
+  Pega o ultimo filho,
+  var = $main = document.queSelector('.main)
+      $main.lasttChild;
+  .nextSibling
+    //pega o proximo irmão
+    var = $main = document.queSelector('.main)
+        $main.nextSibling;
+
+  .previousSibling
+   //pega o irmão anterior
+    var = $main = document.queSelector('.main)
+        $main.previousSibling
+
+  **Propriedades**
+    .nodeType
+      Traz um numero, para cada tipo de node
+    .nodeValue
+      //Pega o valor
+        var = $main = document.queSelector('.main)
+            //Entra no prmeiro filho, dentro dessa node pega o proximo irmao e seu conteudo
+            $main.firsChild.nextSibling.nodeValue
+    .nodeName
+        Traz o nome do nó, ou da tag ou do texto
+
+    **Métodos**
+    .hasAttribute(name)
+      Retorna false ou true se existir algun tributo passado pro parametro
+    .hasAttributes()
+    Retorna false ou true se existir algun tributo 
+
+
+
+
+
+
+
+*/
+
+
