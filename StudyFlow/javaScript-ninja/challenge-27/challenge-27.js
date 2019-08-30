539216db859b84dc6e493f0d0af5195eb7a073be
Rafael Silveira <rafaelssilveira@outlook.com>
2019-08-30 13:52:27 -0300

    Feitos algumas metodos

--- StudyFlow/javaScript-ninja/challenge-27/challenge-27.js
+++ StudyFlow/javaScript-ninja/challenge-27/challenge-27.js
@@ -20,41 +20,76 @@ Crie os seguintes métodos para verificação de tipo:
 - isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
 O método isNull deve retornar `true` se o valor for null ou undefined.
 */
-	
-	
+
+
  'use strict'
 	function DOM (element){
 		this.element = document.querySelectorAll(element)
 	}
-		
-	
+
+
 	DOM.prototype.on = function on(eventType,callback){
 		Array.prototype.forEach.call(this.element,function(element){
 			element.addEventListener(eventType,callback,false)
-			
-		})	
+
+		})
 	}
 	DOM.prototype.off = function off(eventType,callback){
 		Array.prototype.forEach.call(this.element,function(element){
 			element.removeEventListener(eventType,callback,false)
-			
-		})	
+
+		})
 	}
 	DOM.prototype.get = function get(){
 		return	this.element
 	}
-	
+
 	DOM.prototype.forEach= function forEach(){
 		Array.prototype.forEach.apply(this.element, arguments)
-	}
-	
+  }
+  DOM.prototype.map = function map() {
+    Array.prototype.map.apply(this.element, arguments)
+  }
+
+   DOM.prototype.filter = function filter() {
+    Array.prototype.filter.apply(this.element, arguments)
+   }
+   DOM.prototype.reduce = function reduce() {
+     Array.prototype.reduce.apply(this.element, arguments)
+   }
+   DOM.prototype.reduceRight = function reduceRight() {
+     Array.prototype.reduceRight.apply(this.element, arguments)
+   }
+   DOM.prototype.every = function every() {
+     Array.prototype.every.apply(this.element, arguments)
+   }
+
+   DOM.prototype.some = function some() {
+     Array.prototype.some.apply(this.element, arguments)
+   }
+   /*-isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
+   O método isNull deve retornar `true`
+   se o valor
+   for null ou undefined.*/
+   DOM.prototype.is= function is() {
+     Object.prototype.toString.call(this.element, arguments)
+     }
+
+     function isNumber(obj) {
+       return is(obj) === '[object Number]'
+     }
+     console.log(isNumber($a));
+
 	var $a = new DOM ('[data-js="link"]')
-	console.log($a)
-	$a.forEach(function(item){
-		console.log(item.firstChild.nodeValue)
+  console.log($a)
+
+
+
+	$a.filter(function(item){
+		console.log(item.firstChild.nodeValue=="Link 1")//true
 	})
-	
-  
+
+  //forEach, map, filter, reduce, reduceRight, every e some.
 
 	})()
 
