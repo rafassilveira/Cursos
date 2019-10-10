461fb555022a618db1e85942bf3e750f6be6297b
Rafael Silveira <rafaelssilveira@outlook.com>
2019-10-10 13:13:24 -0300

    bug database fixed

--- StudyFlow/rockseat/bootCamp/Modulo2/src/app/controllers/AvailableController.js
+++ StudyFlow/rockseat/bootCamp/Modulo2/src/app/controllers/AvailableController.js
@@ -1,4 +1,5 @@
 const moment = require('moment')
+// variavel padrao do sequilize para manipular datas
 const {
   Op
 } = require('sequelize')
@@ -8,12 +9,17 @@ const {
 
 class AvailableController {
   async index(req, res) {
+    //pegar a data enviando pelo query.params(?), e passar para inteiro
     const date = moment(parseInt(req.query.date))
 
+    //buscar todos os agendamentos, do dia selecionado
     const appointments = await Appointment.findAll({
       where: {
+        //pegar os agedamentos do provider
         provider_id: req.params.provider,
+
         date: {
+          //between precisa de uma data inicial e final
           [Op.between]: [
             date.startOf('day').format(),
             date.endOf('day').format(),
@@ -21,6 +27,7 @@ class AvailableController {
         }
       }
     })
+    //definindo os horarios estaticamente
     const schedule = [
       '08:00',
       '09:00',
@@ -35,16 +42,23 @@ class AvailableController {
       '18:00'
 
     ]
+    //verificar se tem horarios disponivel ou se já passou
+
     const available = schedule.map(time => {
+      //retirar o horario e minuto
       const [hour, minute] = time.split(':')
+      //formatando a hora pra 2019-09-16 08:00:00
       const value = date
         .hour(hour)
         .minute(minute)
         .second(0)
       return {
         time,
+        //retornar um formato legivel
         value: value.format(),
+        //verficar se  horio já passoi do horario atual, retorna boolean
         available: value.isAfter(moment()) &&
+          //vai pecorrer ao horarios e verificar se está ocupado
           !appointments.find(a => moment(a.date).format('HH:mm') === time)
       }
     })
--- StudyFlow/rockseat/bootCamp/Modulo2/src/database/migrations/20191003171648-create-appointments.js
+++ StudyFlow/rockseat/bootCamp/Modulo2/src/database/migrations/20191003171648-create-appointments.js
@@ -1,54 +1,54 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('appointments', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      date: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      //chaves estrangeiras, chaves de relacionamento usuario
-      user_id: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: 'users',
-          key: 'id'
-        },
-        //server para caso mude o id do user atualizar automaticamente
-        onUpdate: 'CASCADE',
-        //server para caso delete o id do user atualizar automaticamente
-        onDelete: 'CASCADE',
-        allowNull: false,
-      },
-       //chaves estrangeiras, chaves de relacionamento provider
-      provider_id: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: 'users',
-          key: 'id'
-        },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
-        allowNull: false,
-      },
-      created_at: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      update_at: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    })
-  },
+    up: (queryInterface, Sequelize) => {
+      return queryInterface.createTable('appointments', {
+            id: {
+              allowNull: false,
+              autoIncrement: true,
+              primaryKey: true,
+              type: Sequelize.INTEGER
+            },
+            date: {
+              allowNull: false,
+              type: Sequelize.DATE,
+            },
+            //chaves estrangeiras, chaves de relacionamento usuario
+            user_id: {
+              type: Sequelize.INTEGER,
+              references: {
+                model: 'users',
+                key: 'id'
+              },
+              //server para caso mude o id do user atualizar automaticamente
+              onUpdate: 'CASCADE',
+              //server para caso delete o id do user atualizar automaticamente
+              onDelete: 'CASCADE',
+              allowNull: false,
+            },
+            //chaves estrangeiras, chaves de relacionamento provider
+            provider_id: {
+              type: Sequelize.INTEGER,
+              references: {
+                model: 'users',
+                key: 'id'
+              },
+              onUpdate: 'CASCADE',
+              onDelete: 'CASCADE',
+              allowNull: false,
+            },
+            created_at: {
+              allowNull: false,
+              type: Sequelize.DATE
+            },
+            updated_at: {
+              allowNull: false,
+              type: Sequelize.DATE
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('appointments');
-  }
-};
+            })
+        },
+
+        down: (queryInterface, Sequelize) => {
+          return queryInterface.dropTable('appointments');
+        }
+    };
